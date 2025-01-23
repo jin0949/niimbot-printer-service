b1010356e172f25f8d461fe0486609e14595bed8
hong <daggax8235@gmail.com>
2025-01-23 16:57:58 +0900

    의도한 데이터로 이미지 출력 되는지 까지 체크

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import asyncio
 import logging
+import json
 
+from src.qr_generator.layout import ImageLayout
+from src.supa_db.supa_db import SupaDB
 from src.supa_realtime.config import DATABASE_URL, JWT
 from src.supa_realtime.realtime_service import RealtimeService
 
 
-async def handle_change(payload):
-    logging.info(f"Database change detected: {payload}")
+class LaundryHandler:
+    def __init__(self):
+        self.supa_api = SupaDB()
+        self.service = RealtimeService(DATABASE_URL, JWT, self.handle_change)
+
+    async def handle_change(self, payload):
+        logging.info(f"Database change detected: {payload}")
+        record = payload['data']['record']
+        laundry_id = record['id']
+        amount = record['amount']
+        requested_by = record['requested_by']
+        user_name = self.supa_api.get_user_name(requested_by)
+
+        for i in range(amount):
+            number = i + 1
+            data = {
+                "id": laundry_id,
+                "number": number
+            }
+            json_data = json.dumps(data)
+            image = ImageLayout.create_qr_image(json_data, f"{user_name} {number}")
+
+
+    async def start(self):
+        await self.service.start_listening()
+
 
 async def main():
     logging.basicConfig(level=logging.INFO)
-    service = RealtimeService(DATABASE_URL, JWT, handle_change)
-    await service.start_listening()
+    handler = LaundryHandler()
+    await handler.start()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
