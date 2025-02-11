c85f4471c98b98c0fdc19421b6a5378675d8c5a9
hong <jinmail0949@gmail.com>
2025-02-11 11:30:59 +0900

    로깅 설정 바꿈 그냥 기본 제공해주는 로깅쓰는걸로 하고 세팅할때 레벨설정 하는걸로.

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,27 +1,48 @@
 import os
 import logging
-
 from logging.handlers import TimedRotatingFileHandler
 
-
-def setup_logger():
+def setup_logger(log_level=logging.INFO):
     log_dir = 'logs'
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
-    logger = logging.getLogger('critical_errors')
-    logger.setLevel(logging.ERROR)
+    # 루트 로거 설정
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
 
-    error_handler = TimedRotatingFileHandler(
-        filename=os.path.join(log_dir, 'critical_errors.log'),
+    # 일반 로그 파일 핸들러
+    service_handler = TimedRotatingFileHandler(
+        filename=os.path.join(log_dir, 'service.log'),
         when='midnight',
         interval=1,
         backupCount=30,
         encoding='utf-8'
     )
-    formatter = logging.Formatter(
-        '\n[%(asctime)s]\nERROR: %(message)s\n' + '-' * 50
+    service_handler.setLevel(log_level)
+    service_formatter = logging.Formatter(
+        '[%(asctime)s] %(levelname)s: %(message)s'
     )
-    error_handler.setFormatter(formatter)
+    service_handler.setFormatter(service_formatter)
+    logger.addHandler(service_handler)
+
+    # 에러 로그 파일 핸들러
+    error_handler = TimedRotatingFileHandler(
+        filename=os.path.join(log_dir, 'error.log'),
+        when='midnight',
+        interval=1,
+        backupCount=30,
+        encoding='utf-8'
+    )
+    error_handler.setLevel(logging.ERROR)  # 에러 레벨 이상만 기록
+    error_formatter = logging.Formatter(
+        '\n[%(asctime)s]\nERROR: %(message)s\n' + '-'*50
+    )
+    error_handler.setFormatter(error_formatter)
     logger.addHandler(error_handler)
-    return logger
+
+    # 콘솔 핸들러
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(log_level)
+    console_handler.setFormatter(service_formatter)
+    logger.addHandler(console_handler)
