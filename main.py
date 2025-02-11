ecaf8b89af0f411b32b6678dc0d654ce2a86f5ab
hong <jinmail0949@gmail.com>
2025-02-11 13:39:36 +0900

    업데이트한 내용들 기반으로 main 코드 업뎃.

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import asyncio
 import os
 import sys
+import logging
+
 from setproctitle import setproctitle
 from dotenv import load_dotenv
 
 from src.niimbot.niimbot_printer import NiimbotPrint
-from src.niimbot.printer_monitor import PrinterMonitor
-from src.niimbot.serial_transport import SerialTransport
 from src.supa_db.supa_db import SupaDB
 from src.supa_realtime.realtime_service import RealtimeService
 from src.utils.logger import setup_logger
+from src.utils.print_test_page import print_test_page
 
 # User configurations
 SERIAL_PORT = "/dev/ttyACM0"
 SERVICE_NAME = "printer-service"
+
+# Set log Level
+LOG_LEVEL = logging.INFO
 
 # Load environment variables
 load_dotenv()
@@ -23,33 +27,23 @@ JWT = os.getenv('JWT')
 
 async def main():
     setproctitle(SERVICE_NAME)
-    logger = setup_logger()
-
-    transport = None
-    printer_monitor = None
+    setup_logger(LOG_LEVEL)
 
     try:
-        transport = SerialTransport(port=SERIAL_PORT)
-        printer = NiimbotPrint(transport=transport)
-
-        printer_monitor = PrinterMonitor(printer, transport, logger)
-        printer_monitor.start()
+        printer = NiimbotPrint(port=SERIAL_PORT)
+        # 첫 출력 공백문제 때문에 테스트 페이지 출력
+        print_test_page(printer)
 
         supa_api = SupaDB(DATABASE_URL, JWT)
         service = RealtimeService(DATABASE_URL, JWT, printer, supa_api)
         await service.start_listening()
 
     except Exception as e:
-        logger.error(
+        logging.error(
             f"CRITICAL: Service initialization failed\n"
             f"Error: {str(e)}"
         )
         sys.exit(1)
-    finally:
-        if printer_monitor:
-            printer_monitor.stop()
-        if transport:
-            transport.close()
 
 
 if __name__ == "__main__":
